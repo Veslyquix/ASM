9798214e56ce28a3235e1e9107755c053f48d02f
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-06-24 18:54:45 -0400

    Commit

--- GenerateMap/GenerateMap.c
+++ GenerateMap/GenerateMap.c
@@ -1,4 +1,5 @@
 #include "gbafe.h"
+#include <string.h>
 // F42C Event2A_MoveToChapter
 // gets chapter ID (sometimes from mem slot 2) 
 // stores this to gChapterData + 0x4A as chapter to go to 
@@ -17,6 +18,19 @@
 void CopyMapPiece(u16 dst[], u8 xx, u8 yy, u8 map_size_x, u8 map_size_y, u16 defaultTile);
 
 extern int NumberOfMapPieces; 
+extern int FrequencyOfObjects_Link; 
+//extern int *GenerateMapRam_Link[]; 
+
+struct GeneratedMapDimensions_Struct
+{
+	u8 min_x; 
+	u8 min_y; 
+	u8 max_x; 
+	u8 max_y; 
+};
+
+extern struct GeneratedMapDimensions_Struct GeneratedMapDimensions;
+
 struct MapPieces_Struct
 {
 	u8 x; 
@@ -34,48 +48,84 @@ struct Map_Struct
 void GenerateMap(struct Map_Struct* dst);
 
 
+int hashCode(const char *str) {
+    int hash = 0;
+
+    for (int i = 0; i < strlen(str); i++) {
+        hash = 31 * hash + str[i];
+    }
+
+    return hash;
+}
+
+// game time initialized -> chapter start events -> when map needs to be displayed, then it is loaded (so if you are faded in, events occur first) 
+
 void GenerateMap(struct Map_Struct* dst)
 {
-	//memset(gGenericBuffer, 0, 1600);
-	//memcpy(gGenericBuffer, DebugMapLabel, 660); // dst, src, size 
-	//
-	//memset(gGenericBuffer+1600, 0, 1600);
-	//Decompress(gGenericBuffer,(void*)gGenericBuffer+1600);
-	//gGenericBuffer[2] = 0; 
-	//memcpy(dst, gGenericBuffer+1600, 1298); // dst, src, size 
-	
-	// 2 bytes are the map's XX / YY 
-	// then it's just SHORTs of the different tileset IDs in a row 
-	//asm("mov r11, r11");
+	// 2 bytes are the map's XX / YY size
+	// then it's just SHORTs of the different tileset IDs in a row as YY << 7 | XX << 2
 	// uncompressed size is 0x512 / #1298
+	extern struct ChapterState gChapterData; 
 
-	dst->x = (NextRN_N(20)+15);
-	dst->y = (NextRN_N(25)+10);
+	// hooked InitChapterMap to update gChapterData._u04 right before LoadChapterMap instead of right after 
+	
+	
+	// GmDataInit 0x80BC81C at BC884 calls SetRandState();
+	// 300534D
+	u16 saveRandState[3]; 
+	//GetRandState(saveRandState);
+	int clock = GetGameClock(); 
+	int t_start = gChapterData._u04;
+	//asm("mov r11, r11"); 
+	if (t_start != clock) { 
+		u16 var[3]; 
+		var[0] = ((t_start-0xF0F0F0F0) & 0xFFFF); // clock at start of chapter 
+		var[1] = (((t_start-0x0F0F0F0F) & 0xFFFF0000)>>16); 
+		var[2] = hashCode(&gChapterData.playerName[0]);
+		SetRandState(var); //! FE8U = (0x08000C4C+1)
+	} 
+	
+	struct GeneratedMapDimensions_Struct dimensions = GeneratedMapDimensions;
+	
+	dst->x = (NextRN_N(dimensions.max_x-dimensions.min_x)+dimensions.min_x); 
+	dst->y = (NextRN_N(dimensions.max_y-dimensions.min_y)+dimensions.min_y);
+	u16 c = 0; 
+	while (((dst->x * dst->y) > 1500) && (c<255)) { // redo if it will exceed max map size 
+		dst->x = (NextRN_N(dimensions.max_x-dimensions.min_x)+dimensions.min_x); 
+		dst->y = (NextRN_N(dimensions.max_y-dimensions.min_y)+dimensions.min_y);
+		c++; 
+	}
+	if (c == 255) { dst->y = 10; }  // if we try 255 times and fail, make height the min of 10. 
 
 	u8 map_size_x = dst->x;
 	u8 map_size_y = dst->y; 
-	
+	//int FrequencyOfObjects_Link; 
 	// creates a randomized map 
 	for (int iy = 0; iy<map_size_y; iy++) {
 		for (int ix = 0; ix < map_size_x; ix++) {
-			u16 value = 0;
-			while (!value) { // never be 0 
-				value = NextRN_N(32) <<7 | (NextRN_N(32)<<2); // I think NextRN_N is 0-indexed, so given 4 it will return max 3 
-			}
-			//dst->data[iy*x+ix] = 0x268 + NextRN_N(3); //value;
-			if (NextRN_N(5) == 0 ) { 
-				//dst->data[iy*x+ix] = value; 
+			//u16 value = 0;
+			//while (!value) { // never be 0 
+				//value = NextRN_N(32) <<7 | (NextRN_N(32)<<2); // I think NextRN_N is 0-indexed, so given 4 it will return max 3 
+			//}
+			//dst->data[iy*x+ix] = value;  // totally random tiles 
+			
+			if (FrequencyOfObjects_Link > NextRN_N(100)) { 
+				
 				CopyMapPiece(dst->data, ix, iy, map_size_x, map_size_y, dst->data[map_size_y*map_size_x+map_size_x]); // bottom right tile as default tile 
 			}  
 			//else { dst->data[iy * map_size_x + iy] = dst->data[map_size_y*map_size_x+map_size_x]; } 
 		
 		}
 	}
+	SetRandState(saveRandState); 
 }
+
+
 
 void CopyMapPiece(u16 dst[], u8 placement_x, u8 placement_y, u8 map_size_x, u8 map_size_y, u16 defaultTile)
 {
 	struct MapPieces_Struct* T = MapPiecesTable[NextRN_N(NumberOfMapPieces)];
+	
 	u8 piece_size_x = (T->x);
 	u8 piece_size_y = (T->y);
 	
@@ -83,10 +133,10 @@ void CopyMapPiece(u16 dst[], u8 placement_x, u8 placement_y, u8 map_size_x, u8 m
 
 	u8 border_y = placement_y;
 	u8 border_x = placement_x;
-	if (placement_y) { // border of 1 tile on left/above 
+	if ((placement_y) && ((piece_size_y > 1)||(piece_size_x > 1))) { // border of 1 tile on left/above 
 		border_y = placement_y - 1; 
 	}
-	if (placement_x) { 
+	if ((placement_x) && ((piece_size_y > 1)||(piece_size_x > 1))) { 
 		border_x = placement_x - 1; 
 	} 
 	for (u8 y = 0; y <= piece_size_y+1; y++) {
@@ -106,7 +156,4 @@ void CopyMapPiece(u16 dst[], u8 placement_x, u8 placement_y, u8 map_size_x, u8 m
 	}
 
 }
-
-
-
 
---
+++ GenerateMap/InitClockAndMap.asm
@@ -0,0 +1,80 @@
+.thumb 
+
+.macro blh to, reg=r3
+  ldr \reg, =\to
+  mov lr, \reg
+  .short 0xf800
+.endm
+
+.equ InitChapterMap, 0x80194BC
+.equ AddSnagsAndWalls, 0x802E3A8
+.equ GetGameClock, 0x8000D28
+.equ SetOtherRNState, 0x8000CDC 
+.equ GetOtherRN, 0x8000CE8 
+.equ InitRN, 0x8000BC8 
+
+.type InitClockAndMap, %function 
+.global InitClockAndMap
+InitClockAndMap: 
+PUSH {r4, lr}  
+mov r4, r0 
+blh GetGameClock
+str r0, [r5, #4] @ vanilla 
+mov r0, r4 
+blh InitChapterMap
+blh AddSnagsAndWalls
+
+mov r2, #0
+pop {r4}  
+pop {r3}
+ldr r3, =0x8030EA5 
+bx r3  
+
+.ltorg 
+
+.type NewInitRNG, %function 
+.global NewInitRNG
+NewInitRNG:
+push {r4, lr} 
+blh GetGameClock
+ldr r1, =0x12345678
+mul r0, r1 
+sub sp, #12 
+mov r4, sp 
+add r1, r4, #4 
+mov r2, #0 
+str r2, [r4, #8] 
+str r2, [r4]
+str r0, [r4] 
+
+@ 
+@mov r0, r4 
+@ldr r0, =0x202BCF0
+@add r0, #0x20 @ tact 
+@str r0, [r4] 
+mov r0, r4 
+blh hashCode @ returns hash'd int in r0 
+
+add sp, #12
+@mov r11, r11 
+blh SetOtherRNState // from 0xAA0 / 0xA20 
+blh GetOtherRN
+blh InitRN 
+
+mov r1, r5 @ vanilla 
+add r1, #0x2E 
+mov r0, #5 
+strb r0, [r1] 
+pop {r4} 
+pop {r0}
+ldr r0, =0x80A94A1
+bx r0 
+
+.ltorg 
+
+
+
+
+
+
+
