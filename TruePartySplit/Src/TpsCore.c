eab73b7823307cff301e8d2d631523adf0624407
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2022-08-05 12:09:04 -0400

    Commit

--- TruePartySplit/Src/TpsCore.c
+++ TruePartySplit/Src/TpsCore.c
@@ -3,28 +3,29 @@
 
 enum { BITS_PER_PARTY_ID = 4 };
 
-#define PARTY_ID(party) (party & ((1 << (BITS_PER_PARTY_ID-1))-1))
-#define PARTY_RM(party) (party & ((1 << (BITS_PER_PARTY_ID-1))))
+#define PARTY_ID(party) (party & ((1 << (BITS_PER_PARTY_ID-1))-1)) // & 0x7 
+#define PARTY_RM(party) (party & ((1 << (BITS_PER_PARTY_ID-1)))) // & 0x8
+
 
 int TpsGetPartyRawByPid(int pid)
 {
-    static int const mask = 0xF; //4 bits 
+    static int const mask = (1 << BITS_PER_PARTY_ID) - 1;
 
-    int const slot = pid >> 1; //divide by 2
-    int const shift = ((pid & 1) ^ 1) << 2; //4 if an even number, 0 otherwise 
+    int const slot = pid / (8 / BITS_PER_PARTY_ID);
+    int const shift = (pid % (8 / BITS_PER_PARTY_ID)) * BITS_PER_PARTY_ID;
 
     return (g_tps_party_array[slot] >> shift) & mask;
 }
 
 void TpsSetPartyRawForPid(int pid, int party)
 {
-    static int const mask = 0xF; //4 bits 
+    static int const mask = (1 << BITS_PER_PARTY_ID) - 1;
 
-    int const slot = pid >> 1; //divide by 2
-    int const shift = ((pid & 1) ^ 1) << 2; //4 if an even number, 0 otherwise 
+    int const slot = pid / (8 / BITS_PER_PARTY_ID);
+    int const shift = (pid % (8 / BITS_PER_PARTY_ID)) * BITS_PER_PARTY_ID;
 
-    g_tps_party_array[slot] &= ~(mask << shift); //clear the bits we are about to set
-    g_tps_party_array[slot] |= (party & mask) << shift; //set the bits
+	g_tps_party_array[slot] &= ~(mask << shift);
+    g_tps_party_array[slot] |= (party & mask) << shift;
 }
 
 int TpsGetPartyByPid(int pid)
@@ -34,11 +35,9 @@ int TpsGetPartyByPid(int pid)
 
 void TpsSetPartyByPid(int pid, int party)
 {
-    static int const mask = (1 << (BITS_PER_PARTY_ID)) - 1;
+    static int const mask = (1 << (BITS_PER_PARTY_ID-1)) - 1;
 
     int val = TpsGetPartyRawByPid(pid);
-
-
 
     val &= ~mask;
     val |= party & mask;
@@ -46,10 +45,12 @@ void TpsSetPartyByPid(int pid, int party)
     TpsSetPartyRawForPid(pid, val);
 }
 
+
+
 int TpsIsDisabledByPid(int pid)
 {
     return !!(TpsGetPartyRawByPid(pid) & (1 << (BITS_PER_PARTY_ID-1)));
-}
+} // !! is 0 if 0, 1 if anything but 0 
 
 void TpsSetDisabledByPid(int pid, int disabled)
 {
@@ -60,6 +61,8 @@ void TpsSetDisabledByPid(int pid, int disabled)
 
     TpsSetPartyRawForPid(pid, val);
 }
+
+
 
 void TpsRefreshUnitAwayBits(void)
 {
@@ -78,6 +81,7 @@ void TpsRefreshUnitAwayBits(void)
 
         int const party = TpsGetPartyRawByPid(unit->pCharacterData->number);
 
+		// party & 0x8 || party & 0x7 
         if (PARTY_RM(party) || PARTY_ID(party) != *g_tps_current_party)
         {
             // Unit is disabled
