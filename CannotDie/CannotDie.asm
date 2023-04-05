d220468a578b74a36782432e4719fc394faecaf6
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2023-04-05 09:31:35 -0400

    Commit

--- CannotDie/CannotDie.asm
+++ CannotDie/CannotDie.asm
@@ -6,10 +6,14 @@
 .endm
 .equ SecondRound, 0x203E152 @ for doubling? 
 	.equ CheckEventId,0x8083da8
+	.equ SetEventId, 0x8083D80
+	.equ EventEngine, 0x800D07C 
+	.equ MemorySlot, 0x30004B8 
 	.equ CurrentRound_ComputeWeaponEffect, 0x802B600 
-.global CannotDieEffect 
-.type CannotDieEffect, %function 
-CannotDieEffect: 
+	.equ QuoteEvent, CannotDieList+4 
+@.global CannotDieEffect 
+@.type CannotDieEffect, %function 
+@CannotDieEffect: 
 mov r1, r5 @ vanilla 
 push {r4-r7, lr} 
 mov r5, r1 @ defender 
@@ -25,10 +29,10 @@ mov     r7, r0
 @mov r7, r3 @battle data
 
 ldr r4, CannotDieList 
-sub r4, #4 
+sub r4, #8 
 
 Loop: 
-add r4, #4
+add r4, #8
 ldr r0, [r4] 
 mov r1, #0 
 sub r1, #1 @ 0xFFFFFFFF terminator 
@@ -61,11 +65,11 @@ cmp r0, #1
 beq Loop 
 AnyFlag: 
 @only activate if damage > current enemy hp-1
-ldrb	r5,[r5,#0x13]
+ldrb	r1,[r5,#0x13]
 @mov	r0,#0x01
 @sub	r5,r0 @ current hp - 1 
 ldrh    r0,[r7,#0x04] @ damage 
-cmp	r5,r0
+cmp	r1,r0
 bgt Break @ if health > damage, exit 
 @ copied bane 
 @ no clue really what this does 
@@ -75,10 +79,15 @@ ldr     r2,[r6]
 lsl     r1,r2,#0xD                @ 0802B42C 0351     
 lsr     r1,r1,#0xD                @ 0802B42E 0B49     
 mov     r0,#0x2          @miss flag     @ 0802B430 2002  
+
+ldrh r3, [r4, #4] @ death quote? 
+cmp r3, #0 
+bne NoMiss 
 orr r1, r0 @ set miss flag 
 mov     r0, #0x40
 lsl     r0, #8           @0x4000, attacker skill activated
 orr     r1, r0
+NoMiss: 
 ldr     r0,=#0xFFF80000                @ 0802B434 4804     
 and     r0,r2                @ 0802B436 4010     
 orr     r0,r1                @ 0802B438 4308     
@@ -89,8 +98,38 @@ bic r0, r1 @ 	BATTLE_HIT_INFO_KILLS_TARGET = (1 << 2),
 str     r0,[r6]                @ 0802B43A 6018   
 
 
-mov r0, #0 
+ldrh r3, [r4, #4] @ death quote? 
+cmp r3, #0 
+beq ZeroDmg
+ldrb	r0,[r5,#0x13]
+sub r0, #1 @ current hp - 1 as dmg (so we survive at 1 hp) 
+b StoreDmg 
+ZeroDmg: 
+mov r0, #0
+StoreDmg:  
 strh r0, [r7, #0x04] @ no damage 
+
+ldrh r0, [r4, #6] @ flag to set 
+blh SetEventId 
+
+
+
+ldrh r3, [r4, #4] @ death quote? 
+cmp r3, #0 
+beq Break 
+ldr r2, =MemorySlot 
+str r3, [r2, #4*2] @ s2 
+
+ldr r0, =CannotDieProc 
+blh ProcFind 
+cmp r0, #0 
+beq DontEnd 
+blh EndProc 
+
+DontEnd: 
+ldr r0, =CannotDieProc 
+mov r1, #3 @ root 3 
+blh New6C 
 
 
 
@@ -106,6 +145,211 @@ cmp r0, #0
 pop {r1} 
 bx r1 
 .ltorg 
+
+
+.equ EndProc, 0x8002D6C 
+.equ BreakProcLoop, 0x08002E94
+.equ ProcFind, 0x08002E9C
+.set New6C,   0x08002C7C 
+
+.global WaitUntilBattleEnds
+.type WaitUntilBattleEnds, %function
+WaitUntilBattleEnds:
+push {r4, lr} 
+mov r4, r0 @ Parent? 
+
+@ 5B9604 efxHPBar calls 523ED which at 524AC calls ShouldDisplayDeathQuote 
+b Error 
+
+ldr r0, =0x859DABC
+blh ProcFind 
+cmp r0, #0 
+beq Error @ Battle has not started 
+
+blh 0x0800D1B0	@BattleEventEngineExists	{U}
+@blh 0x0800D474	@BattleEventEngineExists	{J}
+cmp r0, #0x1
+beq Error
+
+ldr r0, =0x85b9604 
+blh ProcFind 
+cmp r0, #0 
+beq Error 
+mov r1, #0x50 @ hp to display 
+ldr r1, [r0, r1] 
+cmp r1, #1 
+bne Error @ not at 1 hp yet 
+@ efxHPBar 85b9604 + 0x50 is hp to display 
+
+@ldr r1, [r0, #4] @ code cursor 
+@ldr r2, =0x859DAD4 
+@cmp r1, r2 
+@bne Error @ not at the right place yet? 
+
+
+
+mov r0, r4 @  @ parent to break from 
+blh BreakProcLoop
+
+ldr r0, QuoteEvent 
+mov r1, #2 @ battle quote event 
+blh EventEngine 
+
+
+Error: 
+pop {r4}
+pop {r1}
+bx r1 
+.ltorg 
+.global new_8052F24 
+.type new_8052F24, %function 
+new_8052F24:
+push {r4, lr} 
+mov r4, r0 
+blh 0x0800D1B0	@BattleEventEngineExists	{U}
+cmp r0, #0 
+bne ContinueLoop 
+mov r0, r4 
+blh BreakProcLoop 
+
+ContinueLoop: 
+pop {r4} 
+pop {r0} 
+bx r0 
+.ltorg 
+
+.equ GetAISSubjectId, 0x805A16C 
+.equ TryBattleQuote, 0x8050000
+.equ ShouldDisplayDeathQuoteForChar, 0x80835A8
+
+.global ShouldDisplayDeathQuote 
+.type ShouldDisplayDeathQuote, %function 
+ShouldDisplayDeathQuote:
+push {r4, lr} 
+ldr r0, [r5, #0x50] 
+cmp r0, #1 
+bgt ContinueBattle 
+cmp r0, #0 
+beq VanillaBehaviour 
+cmp r0, #1 
+bne ContinueBattle 
+@ we have 1 hp, so check if CannotDieProc is running
+ldr r0, =CannotDieProc 
+blh ProcFind 
+cmp r0, #0 
+beq ContinueBattle 
+
+@ldr r0, =0x85B9378 @ ekrBattle 
+@blh ProcFind 
+@cmp r0, #0 
+@beq ContinueBattle 
+@
+@
+@mov r4, r0 @ CannotDieProc 
+@mov r1, #1 
+@str r1, [r4, #0x54] 
+@mov r1, #0 
+@str r1, [r4, #0x58] 
+@mov r1, #0x1E 
+@strh r1, [r4, #0x2c]
+@mov r11, r11  
+@blh TryBattleQuote
+@@ need to pause battle here or end it depending on its state 
+@
+@
+@
+@b ContinueBattle 
+@ 4FF64 - ekrBattle_Init: ShouldCallBattleQuote 
+
+mov r0, r6 @ 
+blh GetAISSubjectId 
+ldr r1, =0x203E190 @ gBatttleUnitID1
+add r0, r1 
+ldrb r0, [r0] 
+blh ShouldDisplayDeathQuoteForChar 
+cmp r0, #1 
+bne ContinueBattle 
+ldr r0, =efxNearDeathEVTENT @ new version without death fade 
+mov r1, #3 
+blh New6C 
+str r6, [r0, #0x5C] @ based on StartEfxDeadEVTENT at 52DD4
+str r7, [r0, #0x60] 
+ldr r1, =0x8052DF8 @ poin 2017738 
+ldr r1, [r1] 
+mov r0, #1 
+str r0, [r1] @ end of vanilla 
+
+b ContinueBattle 
+
+VanillaBehaviour: 
+@ vanilla - if link battle, don't show death quote 
+ldr r0, =0x804FD5C @ POIN 203E0F0
+ldr r0, [r0] @ gBoolBattleIsLinkArena
+cmp r0, #1
+pop {r4} 
+pop {r3} 
+bx r3 
+.ltorg 
+
+ContinueBattle: 
+pop {r4} 
+pop {r3} 
+ldr r3, =0x80524E5 
+bx r3 
+.ltorg 
+
+.global new_8052EAC
+.type new_8052EAC, %function 
+new_8052EAC:
+PUSH {r4,r5,lr}   @//Procs efxDeadEVTENT CallASM
+SUB SP, #0x4
+MOV r5 ,r0
+blh 0x08056D74
+LSL r0 ,r0 ,#0x18
+ASR r0 ,r0 ,#0x18
+CMP r0, #0x1
+BNE exit_new_8052EAC
+blh 0x08051218   @//EkrGauge_8051218
+blh 0x08051B90
+MOV r0, #0x0
+STR r0,[SP, #0x0]
+LDR r1, =0x2022ca8 @ # pointer:08052F14 -> 02022CA8 (BG0 Map Buffer )
+LDR r2, =0x1000200  @# pointer:08052F18 -> 01000200
+MOV r0, SP
+blh 0x080D1674   @//CPUFastSet
+LDR r0, =0x2000038 @ # pointer:08052F1C -> 02000038
+LDRH r1, [r0, #0x0] @ # pointer:02000038
+LDRH r2, [r0, #0x2] @ # pointer:0200003A
+MOV r0, #0x0
+blh 0x0800148C   @//BG_SetPosition
+MOV r0, #0x1
+MOV r1, #0x0
+MOV r2, #0x0
+blh 0x0800148C   @//BG_SetPosition
+MOV r0, #0x1
+blh 0x08001FAC   @//BG_EnableSyncByMask
+blh 0x08051190   @//EkrGauge_8051190
+LDR r4, =0x203E190 @# pointer:08052F20 -> 0203E190 (gBatttleUnitID1 )
+LDR r0, [r5, #0x5C]
+blh 0x0805A16C   @//GetAISSubjectId r0=@AnimationInterpreter
+ADD r0 ,r0, R4
+LDRB r0, [r0] 
+
+ldr r0, =MemorySlot 
+ldr r0, [r0, #4*2] @ text to display 
+cmp r0, #0 
+beq exit_new_8052EAC 
+blh 0x800D284 @ CallBattleQuoteTextEvents 
+@blh 0x080835DC   @//DisplayDeathQuoteForChar
+MOV r0 ,r5
+blh 0x08002E94   @//Break6CLoop
+exit_new_8052EAC: 
+ADD SP, #0x4
+POP {r4,r5}
+POP {r0}
+BX r0
+.ltorg 
+
 CannotDieList: 
 
 
