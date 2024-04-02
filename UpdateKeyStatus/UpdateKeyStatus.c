6236107d68e365ee5f6d264b969724c6c0942a99
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-04-02 07:15:55 -0400

    Commit

--- UpdateKeyStatus/UpdateKeyStatus.c
+++ UpdateKeyStatus/UpdateKeyStatus.c
@@ -20,40 +20,137 @@ enum
     KEY_BUTTON_R      = (1 << 8),
     KEY_BUTTON_L      = (1 << 9),
 };
+extern int RandomizeInputsFlag_Link; 
+extern int RandomizeInputsSpeed; 
+extern int CheckEventId(int); 
+extern int NextRN_N(int);
+
+u16 HashByte(int number, int max) { 
+	if (!max) { return 0; } 
+	u32 hash = 5381;
+	hash = ((hash << 5) + hash) ^ number;
+	return Mod((u16)hash, max);
+}
+
+
+u16 HashDirection(int dir, int noise) { 
+
+/*
+	switch (dir) { 
+		case KEY_DPAD_RIGHT: { dir = 0; break; } 
+		case KEY_DPAD_LEFT: { dir = 1; break; } 
+		case KEY_DPAD_UP: { dir = 2; break; } 
+		case KEY_DPAD_DOWN: { dir = 3; break; } 
+		default: { dir = 3; break; } 
+	}
+*/
+	dir = (dir&0xF0)>>5; 
+	int num; // first result 
+	u16 directionResults[] = { KEY_DPAD_RIGHT, KEY_DPAD_LEFT, KEY_DPAD_UP, KEY_DPAD_DOWN }; 
+	int size = sizeof(directionResults); 
+	if (dir >= size) { dir = 0x3; } 
+	u16 newList[size]; 
+	
+	for (int i = 0; i < size; i++) { 
+		num = HashByte(noise, size-i); 
+		//num = NextRN_N(size-i); 
+		newList[i] = directionResults[num]; 
+		directionResults[num] = directionResults[(size-i)-1]; 
+	} 
+	return newList[dir]; 
+} 
+
+u16 HashDirection(int dir, int noise) { 
+
+	switch (dir) { 
+		case KEY_BUTTON_A: { dir = 0; break; } 
+		case KEY_BUTTON_B: { dir = 1; break; } 
+		case KEY_BUTTON_R: { dir = 2; break; } 
+		case KEY_BUTTON_L: { dir = 3; break; } 
+		default: { dir = 3; break; } 
+	}
+	int num; // first result 
+	u16 directionResults[] = { KEY_BUTTON_A, KEY_BUTTON_B, KEY_BUTTON_R, KEY_BUTTON_L }; 
+	int size = sizeof(directionResults); 
+	u16 newList[size]; 
+	
+	for (int i = 0; i < size; i++) { 
+		num = HashByte(noise, size-i); 
+		//num = NextRN_N(size-i); 
+		newList[i] = directionResults[num]; 
+		directionResults[num] = directionResults[(size-i)-1]; 
+	} 
+	return newList[dir]; 
+} 
+
 
 // never return the input? 
 // for each bit, turn it off, then turn on one at random? 
 u16 HashBits(u16 number, int max){
 	if (!GetGameClock()) return number; 
+	if (!CheckEventId(RandomizeInputsFlag_Link)) return number; 
+	if (!number) { return number; } 
+	
 	u16 init_num = number; 
-	int value = Mod((GetGameClock() >> 8), 3); // every 4 seconds 
-	if (value == 1) { 
-		if (init_num & KEY_BUTTON_A) { number &= ~KEY_BUTTON_A; number |= KEY_BUTTON_B; } 
-		if (init_num & KEY_BUTTON_B) { number &= ~KEY_BUTTON_B; number |= KEY_BUTTON_A; } 
-		if (init_num & KEY_DPAD_UP) { number &= ~KEY_DPAD_UP; number |= KEY_DPAD_DOWN; } 
-		if (init_num & KEY_DPAD_DOWN) { number &= ~KEY_DPAD_DOWN; number |= KEY_DPAD_UP; } 
-		if (init_num & KEY_DPAD_LEFT) { number &= ~KEY_DPAD_LEFT; number |= KEY_DPAD_RIGHT; } 
-		if (init_num & KEY_DPAD_RIGHT) { number &= ~KEY_DPAD_RIGHT; number |= KEY_DPAD_LEFT; } 
-		if (init_num & KEY_BUTTON_START) { number &= ~KEY_BUTTON_START; number |= KEY_BUTTON_SELECT; } 
-		if (init_num & KEY_BUTTON_SELECT) { number &= ~KEY_BUTTON_SELECT; number |= KEY_BUTTON_START; } 
-		if (init_num & KEY_BUTTON_R) { number &= ~KEY_BUTTON_R; number |= KEY_BUTTON_L; } 
-		if (init_num & KEY_BUTTON_L) { number &= ~KEY_BUTTON_L; number |= KEY_BUTTON_R; } 
-	} 
-	if (value == 2) { 
-		if (init_num & KEY_BUTTON_A) { number &= ~KEY_BUTTON_A; number |= KEY_BUTTON_L; } 
-		if (init_num & KEY_BUTTON_B) { number &= ~KEY_BUTTON_B; number |= KEY_BUTTON_R; } 
-		if (init_num & KEY_DPAD_UP) { number &= ~KEY_DPAD_UP; number |= KEY_DPAD_DOWN; } 
-		if (init_num & KEY_DPAD_DOWN) { number &= ~KEY_DPAD_DOWN; number |= KEY_DPAD_UP; } 
-		if (init_num & KEY_DPAD_LEFT) { number &= ~KEY_DPAD_LEFT; number |= KEY_DPAD_RIGHT; } 
-		if (init_num & KEY_DPAD_RIGHT) { number &= ~KEY_DPAD_RIGHT; number |= KEY_DPAD_LEFT; } 
-		if (init_num & KEY_BUTTON_START) { number &= ~KEY_BUTTON_START; number |= KEY_BUTTON_SELECT; } 
-		if (init_num & KEY_BUTTON_SELECT) { number &= ~KEY_BUTTON_SELECT; number |= KEY_BUTTON_START; } 
-		if (init_num & KEY_BUTTON_R) { number &= ~KEY_BUTTON_R; number |= KEY_BUTTON_B; } 
-		if (init_num & KEY_BUTTON_L) { number &= ~KEY_BUTTON_L; number |= KEY_BUTTON_A; } 
-	} 
+	int totalPossibilities = 5; // possibilities 
+	int time = GetGameClock() >> RandomizeInputsSpeed; // every 4 seconds 
+	int value = HashByte(time, totalPossibilities); 
 	
+	switch (value) { 
+		case 0: { number = (1 << NextRN_N(9)) | KEY_BUTTON_B; break; } // back out of things lol 
+		//case 1: { number = (1 << HashByte((init_num + time), 9)) | KEY_BUTTON_R; break; } // view descriptions 
+		//case 2: { number = (1 << NextRN_N(9)) | KEY_BUTTON_A; break; } // random input and A 
+		case 1: { break; } // number = (1 << NextRN_N(9)); break; } // randomly created input
+		//case 2: { number = init_num << 1; break; } 
+		//case 3: { number = init_num >> 1; break; } 
+		case 2: { 
+			if (init_num & KEY_BUTTON_A) { number &= ~KEY_BUTTON_A; number |= KEY_BUTTON_B; } 
+			if (init_num & KEY_BUTTON_B) { number &= ~KEY_BUTTON_B; number |= KEY_BUTTON_A; } 
+			if (init_num & KEY_DPAD_UP) { number &= ~KEY_DPAD_UP; number |= KEY_DPAD_DOWN; } 
+			if (init_num & KEY_DPAD_DOWN) { number &= ~KEY_DPAD_DOWN; number |= KEY_DPAD_UP; } 
+			if (init_num & KEY_DPAD_LEFT) { number &= ~KEY_DPAD_LEFT; number |= KEY_DPAD_RIGHT; } 
+			if (init_num & KEY_DPAD_RIGHT) { number &= ~KEY_DPAD_RIGHT; number |= KEY_DPAD_LEFT; } 
+			if (init_num & KEY_BUTTON_START) { number &= ~KEY_BUTTON_START; number |= KEY_BUTTON_SELECT; } 
+			if (init_num & KEY_BUTTON_SELECT) { number &= ~KEY_BUTTON_SELECT; number |= KEY_BUTTON_START; } 
+			if (init_num & KEY_BUTTON_R) { number &= ~KEY_BUTTON_R; number |= KEY_BUTTON_L; } 
+			if (init_num & KEY_BUTTON_L) { number &= ~KEY_BUTTON_L; number |= KEY_BUTTON_R; } 
+			break; 
+		} 
+		case 3: { 
+			if (init_num & KEY_BUTTON_A) { number &= ~KEY_BUTTON_A; number |= KEY_BUTTON_L; } 
+			if (init_num & KEY_BUTTON_B) { number &= ~KEY_BUTTON_B; number |= KEY_BUTTON_R; } 
+			if (init_num & KEY_DPAD_UP) { number &= ~KEY_DPAD_UP; number |= KEY_DPAD_DOWN; } 
+			if (init_num & KEY_DPAD_DOWN) { number &= ~KEY_DPAD_DOWN; number |= KEY_DPAD_UP; } 
+			if (init_num & KEY_DPAD_LEFT) { number &= ~KEY_DPAD_LEFT; number |= KEY_DPAD_RIGHT; } 
+			if (init_num & KEY_DPAD_RIGHT) { number &= ~KEY_DPAD_RIGHT; number |= KEY_DPAD_LEFT; } 
+			if (init_num & KEY_BUTTON_START) { number &= ~KEY_BUTTON_START; number |= KEY_BUTTON_SELECT; } 
+			if (init_num & KEY_BUTTON_SELECT) { number &= ~KEY_BUTTON_SELECT; number |= KEY_BUTTON_START; } 
+			if (init_num & KEY_BUTTON_R) { number &= ~KEY_BUTTON_R; number |= KEY_BUTTON_B; } 
+			if (init_num & KEY_BUTTON_L) { number &= ~KEY_BUTTON_L; number |= KEY_BUTTON_A; } 
+			break; 
+		} 
+		case 4: { 
+			if (init_num & KEY_BUTTON_A) { number &= ~KEY_BUTTON_A; number |= KEY_DPAD_UP; } 
+			if (init_num & KEY_BUTTON_B) { number &= ~KEY_BUTTON_B; number |= KEY_DPAD_DOWN; } 
+			if (init_num & KEY_DPAD_UP) { number &= ~KEY_DPAD_UP; number |= KEY_BUTTON_A; } 
+			if (init_num & KEY_DPAD_DOWN) { number &= ~KEY_DPAD_DOWN; number |= KEY_BUTTON_B; } 
+			if (init_num & KEY_DPAD_LEFT) { number &= ~KEY_DPAD_LEFT; number |= KEY_BUTTON_L; } 
+			if (init_num & KEY_DPAD_RIGHT) { number &= ~KEY_DPAD_RIGHT; number |= KEY_BUTTON_R; } 
+			if (init_num & KEY_BUTTON_START) { number &= ~KEY_BUTTON_START; number |= KEY_BUTTON_SELECT; } 
+			if (init_num & KEY_BUTTON_SELECT) { number &= ~KEY_BUTTON_SELECT; number |= KEY_BUTTON_START; } 
+			if (init_num & KEY_BUTTON_R) { number &= ~KEY_BUTTON_R; number |= KEY_DPAD_RIGHT; } 
+			if (init_num & KEY_BUTTON_L) { number &= ~KEY_BUTTON_L; number |= KEY_DPAD_LEFT; } 
+			break; 
+		}
+		default: 
+	} 
+	if (init_num < 4) { number |= (1 << HashByte((init_num + time), 2)); } // if you pressed A or B, also press one of them at random 
 	
-	return number; 
+	// this part didn't work how i wanted it to 
+	if (init_num & 0xF0) { number = HashDirection(init_num, time); } // if you pressed a direction, also press one of them at random 
+	if (init_num & 0x303) { number = HashButton(init_num, time); } // if you pressed a direction, also press one of them at random 
+
+	return number & 0x3FF; 
   //u32 hash = 5381;
   //hash = ((hash << 5) + hash) ^ number;
   //hash = ((hash << 5) + hash) ^ gChapterData.chapterIndex;
