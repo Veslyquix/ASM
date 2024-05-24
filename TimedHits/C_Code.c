1d5890f7aeaf78f73f3333ca1408794a3f7a5d29
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2024-05-24 10:37:32 -0400

    some restructuring

--- TimedHits/C_Code.c
+++ TimedHits/C_Code.c
@@ -3,21 +3,26 @@
 int Mod(int a, int b) PUREFUNC;
 typedef struct {
     /* 00 */ PROC_HEADER;
+	struct Anim* anim; 
 	int timer; 
+	int timer2; 
+	struct BattleHit* currentRound;
+	struct BattleUnit* active_bunit;
+	struct BattleUnit* opp_bunit;
 	u8 hitEarly; 
 	u8 hitOnTime; 
 	u8 roundId; 
 	u8 didBonusDmg; 
-	u8 currentUnitAIS;
 	u8 broke; 
 	u8 loadedImg;
-} SomeProc;
-void LoopSomeProc(SomeProc* proc);
-const struct ProcCmd SomeProcCmd[] =
+	u8 side; 
+} TimedHitsProc;
+void LoopTimedHitsProc(TimedHitsProc* proc);
+const struct ProcCmd TimedHitsProcCmd[] =
 {
-	PROC_NAME("SomeProcName"), 
+	PROC_NAME("TimedHitsProcName"), 
     PROC_YIELD,
-	PROC_REPEAT(LoopSomeProc), 
+	PROC_REPEAT(LoopTimedHitsProc), 
     PROC_END,
 };
 extern void* Press_A_Image; 
@@ -25,24 +30,45 @@ extern void* BattleStar;
 extern u16 gPal_Press_A_Image[];
 extern u16 gPal_BattleStar[];
 extern struct KeyStatusBuffer sKeyStatusBuffer; // 2024C78
-void SomeC_Code(void) { 
-	SomeProc* proc; 
-	proc = Proc_Find(SomeProcCmd); 
+void StartTimedHitsProc(void) { 
+	TimedHitsProc* proc; 
+	proc = Proc_Find(TimedHitsProcCmd); 
 	if (!proc) { 
-	proc = Proc_Start(SomeProcCmd, (void*)3); 
+	proc = Proc_Start(TimedHitsProcCmd, (void*)3); 
 	} 
+	proc->anim = NULL; 
 	proc->broke = false; 
-	proc->roundId = 0xFF; 
+	proc->roundId = 0; 
+	proc->timer = 0; 
+	proc->timer2 = 0; 
+	proc->hitEarly = false; 
+	proc->hitOnTime = false; 
+	proc->didBonusDmg = false;
+	proc->loadedImg = false;
+	proc->side = 0xFF; 
+	proc->currentRound = NULL; 
+	proc->active_bunit = NULL; 
+	proc->opp_bunit = NULL; 
+} 
+
+extern struct BattleHit* GetCurrentRound(int roundID); 
+extern s16 GetAnimRoundType(struct Anim * anim);
+extern const int EnemiesCanDoBonusDamage; 
+void SetCurrentAnimInProc(struct Anim* anim) { 
+	TimedHitsProc* proc; 
+	proc = Proc_Find(TimedHitsProcCmd); 
+	proc->anim = anim; 
+	proc->timer = 0; 
+	asm("mov r11, r11"); 
+	proc->broke = false; 
+	//proc->roundId++; 
+	proc->roundId = anim->nextRoundId-1; 
 	proc->timer = 0; 
 	proc->hitEarly = false; 
 	proc->hitOnTime = false; 
 	proc->didBonusDmg = false;
-	proc->currentUnitAIS = 0;
-	proc->loadedImg = false;
-	// load graphics into obj vram here 
-	//LoadObjUIGfx(); // usually already loaded anyway 
-
-
+	proc->loadedImg = false; // reload after each round 
+	proc->currentRound = GetCurrentRound(proc->roundId); 
 } 
 
 //extern u16 EkrEfxIsUnitHittedNow(int pos); // 80522f4
@@ -76,76 +102,67 @@ const u16 sSprite_Star[] = {
     1,
     OAM0_SHAPE_16x16, 
 	OAM1_SIZE_16x16, 
-	OAM2_CHR(0x010B) // tile number 
+	OAM2_CHR(0x0108) // tile number 
 };
-void BreakOnce(SomeProc* proc) { 
+void BreakOnce(TimedHitsProc* proc) { 
 	if (proc->broke) { return; } 
 	proc->broke = true; 
 	asm("mov r11, r11");
 } 
 
-extern s16 GetAnimRoundType(struct Anim * anim);
-extern const int EnemiesCanDoBonusDamage; 
-extern struct BattleHit* GetCurrentRound(int roundID); 
-void DoStuffIfHit(SomeProc* proc, struct ProcEkrBattle* battleProc, struct ProcEfxHPBar* HpProc, struct BattleHit* round, struct Anim* anim, struct Anim* anim2, u32 keys, int x, int y, int side, int roundid);
-void LoopSomeProc(SomeProc* proc) { 
-	proc->timer++; 
-	u32 keys = sKeyStatusBuffer.newKeys | sKeyStatusBuffer.heldKeys; 
-	int x = 0; 
-	int y = 8 * 5; 
+
+void DoStuffIfHit(TimedHitsProc* proc, struct ProcEkrBattle* battleProc, struct ProcEfxHPBar* HpProc, struct BattleHit* round);
+void LoopTimedHitsProc(TimedHitsProc* proc) { 
+	if (!proc->anim) { return; } 
+	  
 	struct ProcEkrBattle* battleProc = gpProcEkrBattle; 
-	struct Anim* anim1 = NULL; 
+	struct Anim* anim1 = proc->anim; 
 	if (battleProc) { anim1 = battleProc->anim; } 
 	else { return; } 
 	struct Anim* anim2 = GetAnimAnotherSide(anim1);
 	if (!anim2) { return; } 
-	//struct Anim* anim = NULL; 
 	
-
-	int roundId = anim1->nextRoundId > anim2->nextRoundId ? anim1->nextRoundId-1 : anim2->nextRoundId-1; 
-	if (proc->roundId != roundId) { 
-		proc->broke = false; 
-		proc->roundId = roundId; 
-		proc->timer = 0; 
-		proc->hitEarly = false; 
-		proc->hitOnTime = false; 
-		proc->didBonusDmg = false;
-		proc->loadedImg = false;
-	} 
-	struct BattleHit* currentRound = GetCurrentRound(roundId); 
+	proc->timer++;
+	proc->timer2++;
+	int side = GetAnimPosition(anim1); 
+	proc->side = side; 
+	struct BattleHit* currentRound = proc->currentRound; 
 	struct ProcEfxHPBar* HpProc = Proc_Find(ProcScr_efxHPBar); 
-	int side = 0xFF; // side is only correct once the target is hit 
-	if (EkrEfxIsUnitHittedNow(0)) { side = 0; } 
-	if (EkrEfxIsUnitHittedNow(1)) { side = 1; } 
+	DoStuffIfHit(proc, battleProc, HpProc, currentRound); 
+} 
 
+int HitNow(TimedHitsProc* proc) { 
+	return EkrEfxIsUnitHittedNow(proc->side);
 
-	//
-	//DoStuffIfHit(proc, battleProc, HpProc, currentRound, anim, anim2, keys, x+((1^side)*12*8), y, side); 
-	DoStuffIfHit(proc, battleProc, HpProc, currentRound, anim1, anim2, keys, x, y, side, roundId); 
-
-	// hp display 203E1AC
 
 } 
+
 #define AlwaysWork
-void ApplyBonusDamage(struct ProcEfxHPBar* HpProc, struct BattleUnit* active_bunit, struct BattleUnit* opp_bunit, int side, struct BattleHit* round, struct Anim* anim, struct Anim* anim2, int roundid);
-void DoStuffIfHit(SomeProc* proc, struct ProcEkrBattle* battleProc, struct ProcEfxHPBar* HpProc, struct BattleHit* round, struct Anim* anim, struct Anim* anim2, u32 keys, int x, int y, int side, int roundid) { 
-	//side = 1 ^ side; 
-	int hitTime = EkrEfxIsUnitHittedNow(side); 
+void ApplyBonusDamage(TimedHitsProc* proc, struct ProcEfxHPBar* HpProc, struct BattleUnit* active_bunit, struct BattleUnit* opp_bunit, struct BattleHit* round);
+void DoStuffIfHit(TimedHitsProc* proc, struct ProcEkrBattle* battleProc, struct ProcEfxHPBar* HpProc, struct BattleHit* round) { 
+	u32 keys = sKeyStatusBuffer.newKeys | sKeyStatusBuffer.heldKeys; 
+	int side = proc->side; 
+	int x = 12 * 8; 
+	int y =  5 * 8;
+	struct BattleUnit* active_bunit = gpEkrBattleUnitLeft; 
+	struct BattleUnit* opp_bunit = gpEkrBattleUnitRight; 
+	if (!side) { 
+		active_bunit = gpEkrBattleUnitRight; 
+		opp_bunit = gpEkrBattleUnitLeft;
+	} 
+	proc->active_bunit = active_bunit; 
+	proc->opp_bunit = opp_bunit; 
+	int hitTime = HitNow(proc); 
 	if (hitTime) { 
-		struct BattleUnit* active_bunit = gpEkrBattleUnitLeft; 
-		struct BattleUnit* opp_bunit = gpEkrBattleUnitRight; 
-		if (!side) { 
-			active_bunit = gpEkrBattleUnitRight; 
-			opp_bunit = gpEkrBattleUnitLeft;
-		} 
+
 		if (!EnemiesCanDoBonusDamage && (UNIT_FACTION(&active_bunit->unit) == FACTION_RED)) { return; } 
 		if (!proc->loadedImg) {
+			proc->timer2 = 0; 
 			Copy2dChr(&Press_A_Image, (void*)0x06012000, 8, 4);
-			Copy2dChr(&BattleStar, (void*)0x06012160, 2, 2);
+			Copy2dChr(&BattleStar, (void*)0x06012100, 2, 2);
 			proc->loadedImg = true;
 		}
-		x = x+((side ^ 1)*12*8);
-		//BreakOnce(proc);
+		x = x+((side)*4*8);
 		if (keys & A_BUTTON) { 
 			proc->hitOnTime = true; 
 		} 
@@ -155,27 +172,27 @@ void DoStuffIfHit(SomeProc* proc, struct ProcEkrBattle* battleProc, struct ProcE
 			proc->didBonusDmg = true; 
 			//PlaySFX(int songid, int volume, int locate, int type)
 			PlaySFX(0x13e, 0x100, 120, 1); // locate is side for stereo? 
-			ApplyBonusDamage(HpProc, active_bunit, opp_bunit, side, round, anim, anim2, roundid); 
+			ApplyBonusDamage(proc, HpProc, active_bunit, opp_bunit, round); 
 		} 
 		#endif 
 		#ifndef AlwaysWork
 		if (proc->hitOnTime && (!proc->hitEarly)) { 
 		#endif 
-			int clock = GetGameClock(); // proc->timer; 
+			//int clock = GetGameClock(); // proc->timer; 
+			int clock = proc->timer2; 
 			//ApplyPalettes(gPal_BattleStar, 14+16, 0x10);
 			int oam2 = OAM2_PAL(14) | OAM2_LAYER(0); //OAM2_CHR(0);
 			x += Mod(clock, 8) >> 1; 
-			y -= Mod(clock, 32); 
+			y -= clock; 
 			//if (y < 40) { y = 40; } 
 			PutSprite(2, OAM1_X(x + 0x200), OAM0_Y(y + 0x100), sSprite_Star, oam2); 
-			
-			//PutSprite(2, OAM1_X(x + 0x200), OAM0_Y(y + 0x100), sSprite_HitInput, 0); 
+
 			#ifndef AlwaysWork 
 			if (!proc->didBonusDmg) { 
 			proc->didBonusDmg = true; 
 			//PlaySFX(int songid, int volume, int locate, int type)
 			PlaySFX(0x13e, 0x100, 120, 1); // locate is side for stereo? 
-			ApplyBonusDamage(HpProc, active_bunit, opp_bunit, side, round, anim, anim2, roundid); 
+			ApplyBonusDamage(proc, HpProc, active_bunit, opp_bunit, round); 
 			} 
 			#endif 
 		#ifndef AlwaysWork
@@ -217,8 +234,10 @@ void AdjustAllRounds(int id, int amount) {
 }
 
 extern s16 gEfxHpLutOff[]; // 203e152 B gEfxHpLutOff
-void ApplyBonusDamage(struct ProcEfxHPBar* HpProc, struct BattleUnit* active_bunit, struct BattleUnit* opp_bunit, int side, struct BattleHit* round, struct Anim* anim, struct Anim* anim2, int roundid) { 
+void ApplyBonusDamage(TimedHitsProc* proc, struct ProcEfxHPBar* HpProc, struct BattleUnit* active_bunit, struct BattleUnit* opp_bunit, struct BattleHit* round) { 
 	//if (!HpProc->post) { return; } 
+	int side = proc->side; 
+	//int roundId = proc->roundId; 
 	int damage = GetBonusDamageAmount(active_bunit, opp_bunit); 
 	int hp = round->hpChange + damage; 
 	//int hp = opp_bunit->unit.curHP - damage;
@@ -253,9 +272,8 @@ void ApplyBonusDamage(struct ProcEfxHPBar* HpProc, struct BattleUnit* active_bun
 		//gpProcEkrBattle->end = true; // does nothing 
 		//gEkrBattleEndFlag = true; // immediately ends without waiting for anything 
 		//NewEkrbattleending(); // crashes 
-
-		anim2->nextRoundId = 8; // seems to work for now see GetAnimNextRoundType
-		anim->nextRoundId = 8; // seems to work for now see GetAnimNextRoundType
+		proc->anim->nextRoundId = 8; // seems to work for now see GetAnimNextRoundType
+		GetAnimAnotherSide(proc->anim)->nextRoundId = 8; 
 
 		//gBanimDoneFlag[0] = true; 
 		//gBanimDoneFlag[1] = true; // doesn't stop the counter attack 
--- TimedHits/TimedHits.asm
+++ TimedHits/TimedHits.asm
@@ -1,16 +1,12 @@
 
-@ copied Huichelaar's HookAttack from battle anim numbers as a base for this test 
-
-@ Hooked at 0x52304. Displays numbers for most attacks. Args:
-@   r0: Recipient's AIS. Can be initiator if devil effect.
 .thumb
 
-.global TestFunc
-.type TestFunc, %function 
-TestFunc: 
+.global CallStartTimedHitsProc
+.type CallStartTimedHitsProc, %function 
+CallStartTimedHitsProc: 
 push  {r14}
 
-bl SomeC_Code
+bl StartTimedHitsProc
 
 ldr r0, =0x8C00008
 mov r10, r0 
@@ -21,6 +17,25 @@ str r0, [r1]
 
 pop {r3} 
 bx r3 
+.ltorg 
+
+.global InitCurrentAnimForTimedHitsProc
+.type InitCurrentAnimForTimedHitsProc, %function 
+InitCurrentAnimForTimedHitsProc:
+push {lr} 
+push {r1} 
+mov r0, r7 @ anim 
+bl SetCurrentAnimInProc
+pop {r1} 
+mov r0, #0x40 
+and r0, r1 
+pop {r3} 
+cmp r0, #0 
+beq Exit2 
+ldr r3, =0x805967d 
+bx r3 
+Exit2: 
+ldr r3, =0x80596cd
 .ltorg 
 
 @ skillsys changes the size of rounds, so we use this function instead of gBattleHitArray[]
@@ -34,4 +49,12 @@ ldr   r1, =0x802aec4      @ &Battle buffer.
 ldr   r1, [r1]
 add   r0, r1          @ Current round in battle buffer.
 bx lr 
+.ltorg 
+
+
+
+
+
+
+
 
