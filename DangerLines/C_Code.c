244667482fd4eaf0927a476f81e4e55c8a3351b1
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-07-03 13:33:07 -0400

    fixed

--- DangerLines/C_Code.c
+++ DangerLines/C_Code.c
@@ -2,7 +2,10 @@
 
 #define brk asm("mov r11, r11");
 extern u8 DangerLinesBuffer[];
-#define DangerLinesBufferSize 0x3A00
+#define DangerLinesBufferSize 0x3000
+
+extern int ShakeIt;
+extern int Pal_4th;
 
 int IsUnitInvalid(struct Unit * unit)
 {
@@ -25,20 +28,23 @@ int IsUnitInvalid(struct Unit * unit)
     return false;
 }
 
-int _log2(int n)
-{
-    int result = 0;
-    while (n >>= 1)
-        result++;
-    return result;
-}
-
 #define enemySize (50 + 7) >> 3
 #define US_BIT_SHAKE (1 << 24)
+#define US_BIT_PAL (1 << 27)
 
 void RemoveEnemyShaking(void)
 {
     struct Unit * unit;
+    int unitState = 0;
+    if (ShakeIt)
+    {
+        unitState |= US_BIT_SHAKE;
+    }
+    if (Pal_4th)
+    {
+        unitState |= US_BIT_PAL;
+    }
+
     for (int i = 0x80; i < 0xC0; ++i)
     {
         unit = GetUnit(i);
@@ -46,96 +52,100 @@ void RemoveEnemyShaking(void)
         {
             continue;
         }
-        unit->state &= ~US_BIT_SHAKE;
+
+        unit->state &= ~unitState;
     }
     RefreshUnitSprites();
 }
+
+// set break points on the buffers ?
+// [0x201c8d0..0x201c8d0+0x3000]!!
 
 void UpdateIconsOnEnemiesWhoCanAttackTile(void)
 {
     int x = gBmSt.playerCursor.x;
     int y = gBmSt.playerCursor.y;
-    SetLastCoords(x, y);               // vanilla
-    SetWorkingBmMap(gBmMapMovement);   // vanilla
-    u8 savedUnitId = gBmMapUnit[y][x]; // when hovering over friendly units, it doesn't update properly ?
-    gBmMapUnit[y][x] = 0;              // also sometimes ram gets wiped or fails atm
+    SetLastCoords(x, y);             // vanilla
+    SetWorkingBmMap(gBmMapMovement); // vanilla
+    u8 savedUnitId = gBmMapUnit[y][x];
+    gBmMapUnit[y][x] = 0;
+
+    int unitState = 0;
+    if (ShakeIt)
+    {
+        unitState |= US_BIT_SHAKE;
+    }
+    if (Pal_4th)
+    {
+        unitState |= US_BIT_PAL;
+    }
 
     struct Unit * unit;
     RemoveEnemyShaking();
 
     int deploymentID;
-    u8 * buf = &DangerLinesBuffer[y * (gBmMapSize.y - 1) * enemySize];
-    for (int i = 0; i < enemySize; i++) // i is the current byte
+    u8 * row = &DangerLinesBuffer[y * gBmMapSize.x * enemySize];
+
+    for (int i = 0; i < enemySize; i++) // Loop over bytes
     {
+        u8 byte = row[(x * enemySize) + i];
         for (int bit = 0; bit < 8; bit++)
         {
-
-            deploymentID = buf[(x * enemySize) + i] & (1 << bit);
-            if (!deploymentID)
+            if (!(byte & (1 << bit)))
             {
                 continue;
             }
+            deploymentID = 0x80 + ((i << 3) + bit); // i*8 + bit
 
-            unit = GetUnit(_log2(deploymentID) + 0x80 + (i << 3)); // deployment id per byte, so add (8 * i)
-            // if ((_log2(deploymentID) + 0x80 + (i << 3)) == 0x97)
-            // {
-            // brk;
-            // }
+            unit = GetUnit(deploymentID);
             if (IsUnitInvalid(unit))
             {
                 continue; // not a unit
             }
 
-            unit->state |= US_BIT_SHAKE;
+            unit->state |= unitState;
         }
     }
+
     gBmMapUnit[y][x] = savedUnitId;
     RefreshUnitSprites();
 }
 
 void CopyAttackRangeIntoBuffer(int i, int xSize, int ySize)
 {
-    int deploymentID = 0x80 + i;
+    // register int dummy_r12 __asm__("r12");
+    // int deploymentID = 0x80 + i;
     i &= 0x3F;
     int byteID = i >> 3; // Which byte of the buffer to write to
     i &= 7;
     i = 1 << i; // Deployment ID - Bit to set at that byte
     u8 * buf;
-    u8 * rangeMapY;
+    // u8 * rangeMapY;
 
     for (int y = 0; y < ySize; y++)
     {
-        rangeMapY = gBmMapRange[y];
-        buf = &DangerLinesBuffer[y * ySize * enemySize];
+        // rangeMapY = gBmMapRange[y];
+        buf = &DangerLinesBuffer[y * xSize * enemySize];
         for (int x = 0; x < xSize; x++)
         {
-            // if (x == 2 && y == 15 && deploymentID == 0x97)
-            // {
-            // brk;
-            // }
-            if (!rangeMapY[x])
+            if (!gBmMapRange[y][x])
             {
-                buf[(x * enemySize) + byteID] &= ~i; // dunno why needed atm
                 continue;
             }
+
             buf[(x * enemySize) + byteID] |= i;
         }
     }
 }
 
-void ClearDangerLineBuffer(void)
-{
-    CpuFill16(0, DangerLinesBuffer, DangerLinesBufferSize);
-}
-
 void GenerateDangerLineRange(void)
 {
     brk;
+    // register int dummy_r12 __asm__("r12");
     u8 savedUnitId;
-    int xSize = gBmMapSize.x - 1;
-    int ySize = gBmMapSize.y - 1;
-    ClearDangerLineBuffer();
-    // BmMapFill(gBmMapRange, 0);
+    int xSize = gBmMapSize.x; //- 1;
+    int ySize = gBmMapSize.y; //- 1;
+    CpuFill16(0, DangerLinesBuffer, DangerLinesBufferSize);
 
     for (int i = 0x80; i < 0xC0; ++i) // Enemy only
     {
@@ -154,12 +164,6 @@ void GenerateDangerLineRange(void)
         gBmMapUnit[unit->yPos][unit->xPos] = 0;
 
         SetWorkingBmMap(gBmMapRange);
-
-        // Apply unit's range to range map
-        // if (i == 0x97)
-        // {
-        // brk;
-        // }
         GenerateUnitCompleteAttackRange(unit);
         CopyAttackRangeIntoBuffer(i & 0x3F, xSize, ySize);
 
