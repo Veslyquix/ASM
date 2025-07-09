256299ef3e3b5f00fa3cb607cbf2a23834f00ed8
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-07-09 07:11:55 -0400

    Add safety check to PutSprite for ExtendDescBox

--- FE-CLib_2024/C_Code.c
+++ FE-CLib_2024/C_Code.c
@@ -1,4 +1,4 @@
-#include "c_code.h"
+#include "C_Code.h"
 void ComputeBattleUnitAvoidRate(struct BattleUnit * bu)
 {
     bu->battleAvoidRate = (bu->battleSpeed * 2) + bu->terrainAvoid + (bu->unit.lck);
