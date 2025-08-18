3dde40558ac4bbf487d454969876e2ca675e06cc
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2025-08-18 15:07:42 -0400

    some progress

--- BGMWindow/C_Code.c
+++ BGMWindow/C_Code.c
@@ -92,10 +92,28 @@ void TerrainDisplay_Init(struct PlayerInterfaceProc * proc) // start
 }
 
 // BGMWindow
+extern int GetCurrentBgmSong(void);
+extern int GetCurrentMapMusicIndex(void);
 
+const char * GetPlayingBGM(void)
+{
+    int id = GetCurrentBgmSong() - 1;
+    const char * str = GetStringFromIndex(gSoundRoomTable[id].nameTextId);
+    return str;
+}
+
+const char * GetNextNotificationStr(struct PlayerInterfaceProc * proc)
+{
+    const char * str = "";
+
+    str = GetPlayingBGM();
+    return str;
+}
+
+extern struct Font * gActiveFont;
 void BGMWindow_Init(struct PlayerInterfaceProc * proc)
 {
-    InitTextDb(proc->texts, 3);
+
     proc->burstUnitId = 0;
     proc->hideContents = false;
     proc->showHideClock = 0;
@@ -103,8 +121,21 @@ void BGMWindow_Init(struct PlayerInterfaceProc * proc)
     proc->hBurst = 0;
     proc->isRetracting = false;
 
-    char * str = "This is some text";
-    Text_InsertDrawString(&proc->texts[0], GetStringTextCenteredPos(64, str), TEXT_COLOR_SYSTEM_WHITE, str);
+    const char * str = GetNextNotificationStr(proc);
+    int len = GetStringTextLen(str);
+    int tileWidth = (len + 15) >> 3;
+    // brk;
+    // InitTextDb(proc->texts, pTextWidth[0]);
+    // InitTextDb(proc->texts, tileWidth);
+    // int chr_counter = gActiveFont->chr_counter;
+    // brk;
+    // gActiveFont->chr_counter = (((uintptr_t)gActiveFont->vramDest & 0x1FFFF) >> 5) + 0x100;
+    InitText(proc->texts, tileWidth);
+    // gActiveFont->chr_counter = chr_counter;
+
+    // Text_InsertDrawString(&proc->texts[0], GetStringTextCenteredPos(64, str), TEXT_COLOR_SYSTEM_WHITE, str);
+    Text_InsertDrawString(
+        &proc->texts[0], GetStringTextCenteredPos(tileWidth * 8 + 8, str), TEXT_COLOR_SYSTEM_WHITE, str);
 
     return;
 }
@@ -116,10 +147,10 @@ void BGMWindow_Loop_OnSideChange(struct PlayerInterfaceProc * proc)
     proc->showHideClock = 0;
     proc->hideContents = true;
 
-    proc->cursorQuadrant = GetCursorQuadrant();
+    // proc->cursorQuadrant = GetCursorQuadrant();
 
-    quadrant = GetWindowQuadrant(
-        sPlayerInterfaceConfigLut[proc->cursorQuadrant].xGoal, sPlayerInterfaceConfigLut[proc->cursorQuadrant].yGoal);
+    // quadrant = GetWindowQuadrant(
+    // sPlayerInterfaceConfigLut[proc->cursorQuadrant].xGoal, sPlayerInterfaceConfigLut[proc->cursorQuadrant].yGoal);
 
     // struct PlayerInterfaceProc * tiProc;
     // tiProc = Proc_Find(gProcScr_TerrainDisplay);
@@ -132,7 +163,7 @@ void BGMWindow_Loop_OnSideChange(struct PlayerInterfaceProc * proc)
     // }
     // }
 
-    proc->windowQuadrant = quadrant;
+    // proc->windowQuadrant = quadrant;
     proc->windowQuadrant = 0;
 
     BGMWindowDraw(proc);
@@ -155,110 +186,82 @@ void BGMWindow_Loop_OnSideChange(struct PlayerInterfaceProc * proc)
 #define gUiWindowX_Size 12
 #define gUiWindowY_Size 6
 
-void BGMWindow_808D514(int quadrant, int param_2, int param_3)
+void BGMWindow_808D514(int quadrant, int height, int width) // originally param3 would be line 0 or line 1
 {
-    int x = sPlayerInterfaceConfigLut[quadrant].xGoal;
-    int y = sPlayerInterfaceConfigLut[quadrant].yGoal;
 
-    // if ((x < 0) && (y < 0))
-    // {
-    TileMap_FillRect(gBG1TilemapBuffer, gUiWindowX_Size, gUiWindowY_Size, 0);
-    TileMap_FillRect(gBG0TilemapBuffer, gUiWindowX_Size, gUiWindowY_Size, 0);
+    TileMap_FillRect(gBG1TilemapBuffer, width, gUiWindowY_Size, 0);
+    TileMap_FillRect(gBG0TilemapBuffer, width, gUiWindowY_Size, 0);
 
     TileMap_CopyRect(
-        gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 6 - param_2)), gBG1TilemapBuffer, gUiWindowX_Size,
-        param_2);
+        gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 6 - height)), gBG1TilemapBuffer, width, height);
     TileMap_CopyRect(
-        gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 8 - param_2)), gBG0TilemapBuffer, gUiWindowX_Size,
-        param_2);
-    // }
-
-    // if ((x > 0) && (y < 0))
-    // {
-    // TileMap_FillRect(gBG1TilemapBuffer + TILEMAP_INDEX(gUiScratchX, 0), gUiWindowX_Size, gUiWindowY_Size, 0);
-    // TileMap_FillRect(gBG0TilemapBuffer + TILEMAP_INDEX(gUiScratchX, 0), gUiWindowX_Size, gUiWindowY_Size, 0);
-
-    // TileMap_CopyRect(
-    // gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 6 - param_2)),
-    // gBG1TilemapBuffer + TILEMAP_INDEX(gUiScratchX, 0), gUiWindowX_Size, param_2);
-    // TileMap_CopyRect(
-    // gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 8 - param_2)),
-    // gBG0TilemapBuffer + TILEMAP_INDEX(gUiScratchX, 0), gUiWindowX_Size, param_2);
-    // }
-
-    // if ((x < 0) && (y > 0))
-    // {
-
-    // TileMap_FillRect(gBG1TilemapBuffer + TILEMAP_INDEX(0, gUiScratchY + 4), gUiWindowX_Size, gUiWindowY_Size, 0);
-    // TileMap_FillRect(gBG0TilemapBuffer + TILEMAP_INDEX(0, gUiScratchY + 4), gUiWindowX_Size, gUiWindowY_Size, 0);
-
-    // TileMap_CopyRect(
-    // gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY),
-    // gBG1TilemapBuffer + 0x1C0 + 0x20 * (({ (1 - param_3) * 2 + 20; }) - param_2) - 0x1C0, gUiWindowX_Size,
-    // param_2);
-    // TileMap_CopyRect(
-    // gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY + 2),
-    // gBG0TilemapBuffer + 0x1C0 + 0x20 * (({ (1 - param_3) * 2 + 20; }) - param_2) - 0x1C0, gUiWindowX_Size,
-    // param_2);
-    // }
-
-    if ((x > 0) && (y > 0) && 0)
-    {
-        TileMap_FillRect(
-            gBG1TilemapBuffer + TILEMAP_INDEX(gUiScratchX, gUiScratchY), gUiWindowX_Size, gUiWindowY_Size, 0);
-        TileMap_FillRect(
-            gBG0TilemapBuffer + TILEMAP_INDEX(gUiScratchX, gUiScratchY), gUiWindowX_Size, gUiWindowY_Size, 0);
-
-        TileMap_CopyRect(
-            gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 6 - param_2)), gBG1TilemapBuffer,
-            gUiWindowX_Size, param_2);
-        TileMap_CopyRect(
-            gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 8 - param_2)), gBG0TilemapBuffer,
-            gUiWindowX_Size, param_2);
-
-        // TileMap_CopyRect(
-        // gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY),
-        // gBG1TilemapBuffer + TILEMAP_INDEX(gUiScratchX, gUiScratchY + 4) +
-        // 0x20 * (({ (1 - param_3) * 2 + 20; }) - param_2) - TILEMAP_INDEX(0, gUiScratchY + 4),
-        // gUiWindowX_Size, param_2);
-        // TileMap_CopyRect(
-        // gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY + 2),
-        // gBG0TilemapBuffer + TILEMAP_INDEX(gUiScratchX, gUiScratchY + 4) +
-        // 0x20 * (({ (1 - param_3) * 2 + 20; }) - param_2) - TILEMAP_INDEX(0, gUiScratchY + 4),
-        // gUiWindowX_Size, param_2);
-    }
+        gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, (gUiScratchY + 8 - height)), gBG0TilemapBuffer, width, height);
 
     BG_EnableSyncByMask(BG0_SYNC_BIT | BG1_SYNC_BIT);
+}
 
-    return;
+int GetBGMWindowWidth(struct PlayerInterfaceProc * proc)
+{
+    return proc->texts[0].tile_width + 1;
+}
+
+int GetBGMWindowHeight(struct PlayerInterfaceProc * proc)
+{
+    return 4;
+}
+
+void BGMWindowClean(struct PlayerInterfaceProc * proc)
+{
+    int w = GetBGMWindowWidth(proc);
+    int h = GetBGMWindowHeight(proc);
+    int x = 0;
+    int y = 0;
+
+    // DrawUiFrame(BG_GetMapBuffer(1), x, y, w, h, TILEREF(0, 0), 0);
+
+    ClearUiFrame(BG_GetMapBuffer(0), x, y, w, h);
+    ClearUiFrame(BG_GetMapBuffer(1), x, y, w, h);
+    BG_EnableSyncByMask(BG0_SYNC_BIT | BG1_SYNC_BIT);
 }
 
 void BGMWindowDraw(struct PlayerInterfaceProc * proc)
 {
-    TileMap_FillRect(gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY), 11, 9, 0);
-    TileMap_FillRect(gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY + 2), 11, 9, 0);
+    int w = GetBGMWindowWidth(proc);
+    int h = GetBGMWindowHeight(proc);
+    int x = 0;
+    int y = 0;
 
-    if (proc->unitClock == 0)
-    {
-        CallARM_FillTileRect(
-            gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY), gTSA_GoalBox_OneLine, TILEREF(0x0, 1));
-        PutText(proc->texts, gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 2, gUiScratchY + 3));
-    }
+    DrawUiFrame(BG_GetMapBuffer(1), x, y, w, h, TILEREF(0, 0), 0);
 
-    if (proc->unitClock == 1)
-    {
-        CallARM_FillTileRect(
-            gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY), gTSA_GoalBox_TwoLines, TILEREF(0x0, 1));
-        PutText(&proc->texts[0], gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 2, gUiScratchY + 3));
-        PutText(&proc->texts[1], gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 2, gUiScratchY + 5));
-    }
+    ClearUiFrame(BG_GetMapBuffer(0), x, y, w, h);
+
+    PutText(&proc->texts[0], gBG0TilemapBuffer + TILEMAP_INDEX(gUiScratchX + 0, gUiScratchY + 1));
+    BG_EnableSyncByMask(BG0_SYNC_BIT | BG1_SYNC_BIT);
+
+    // TileMap_FillRect(gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY), 11, 9, 0);
+    // TileMap_FillRect(gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY + 2), 11, 9, 0);
+
+    // if (proc->unitClock == 0)
+    // {
+    // CallARM_FillTileRect(
+    // gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY), gTSA_GoalBox_OneLine, TILEREF(0x0, 1));
+    // PutText(proc->texts, gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 2, gUiScratchY + 3));
+    // }
+
+    // if (proc->unitClock == 1)
+    // {
+    // CallARM_FillTileRect(
+    // gUiTmScratchB + TILEMAP_INDEX(gUiScratchX + 1, gUiScratchY), gTSA_GoalBox_TwoLines, TILEREF(0x0, 1));
+    // PutText(&proc->texts[0], gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 2, gUiScratchY + 3));
+    // PutText(&proc->texts[1], gUiTmScratchA + TILEMAP_INDEX(gUiScratchX + 2, gUiScratchY + 5));
+    // }
 }
 
 void BGMWindow_Loop_SlideIn(struct PlayerInterfaceProc * proc)
 {
-    int unk = sGoalSlideInWidthLut[proc->showHideClock];
+    int height = sGoalSlideInWidthLut[proc->showHideClock];
 
-    BGMWindow_808D514(proc->cursorQuadrant, unk, proc->unitClock);
+    // BGMWindow_808D514(proc->cursorQuadrant, height, GetBGMWindowWidth(proc));
 
     proc->showHideClock++;
 
@@ -275,13 +278,13 @@ void BGMWindow_Loop_SlideIn(struct PlayerInterfaceProc * proc)
 
 void BGMWindow_Loop_SlideOut(struct PlayerInterfaceProc * proc)
 {
-    int unk;
+    int height;
 
     proc->hideContents = true;
 
-    unk = sGoalSlideOutWidthLut[proc->showHideClock];
+    height = sGoalSlideOutWidthLut[proc->showHideClock];
 
-    BGMWindow_808D514(proc->cursorQuadrant, unk, proc->unitClock);
+    // BGMWindow_808D514(proc->cursorQuadrant, height, GetBGMWindowWidth(proc));
 
     proc->showHideClock++;
 
@@ -297,7 +300,7 @@ void BGMWindow_Loop_SlideOut(struct PlayerInterfaceProc * proc)
 
     return;
 }
-
+extern int BGMWindow_DisplayFrames;
 void BGMWindow_Loop_Display(struct PlayerInterfaceProc * proc)
 {
     proc->xCursorPrev = proc->xCursor;
@@ -305,6 +308,13 @@ void BGMWindow_Loop_Display(struct PlayerInterfaceProc * proc)
 
     proc->xCursor = gBmSt.playerCursor.x;
     proc->yCursor = gBmSt.playerCursor.y;
+
+    proc->showHideClock++;
+    if (proc->showHideClock > BGMWindow_DisplayFrames)
+    {
+        BGMWindowClean(proc);
+        Proc_End(proc);
+    }
 
     if (proc->xCursor == proc->xCursorPrev && proc->yCursor == proc->yCursorPrev)
     {
@@ -328,9 +338,9 @@ void BGMWindow_Loop_Display(struct PlayerInterfaceProc * proc)
         }
     }
 
-    proc->isRetracting = true;
+    // proc->isRetracting = true;
 
-    Proc_Break(proc);
+    // Proc_Break(proc);
 
     return;
 }
