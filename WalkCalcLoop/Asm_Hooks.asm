fadba6c2126c152d2b268a33ea60d48175f82e8d
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-01-12 10:31:56 -0500

    wip

--- WalkCalcLoop/Asm_Hooks.asm
+++ WalkCalcLoop/Asm_Hooks.asm
@@ -5,9 +5,13 @@
 .type WalkCalcLoop_Hook, %function 
 WalkCalcLoop_Hook: 
 push  {r14}
-push {r2-r3} 
+mov r1, r12 
+push {r1-r3} 
 cmp r0, #0 
 bne InterruptMovement 
+mov r0, r7 @ unit 
+mov r1, r3 @ current x 
+mov r2, r4 @ current y 
 bl WalkCalcLoop
 cmp r0, #0 
 bne InterruptMovement 
@@ -18,10 +22,10 @@ mov r0, #4
 strb r0, [r5, #1] 
 mov r0, #0x1e 
 strb r0, [r6, #0x11] 
-pop {r2-r3} @ r2 & r3 need to be preserved after returning 
+pop {r1-r3} @ r2 & r3 need to be preserved after returning 
 strb r3, [r6, #0xE] 
 strb r4, [r6, #0xF] 
-
+mov r12, r1 
 pop {r1} 
 ldr r1, =0x801A8DD 
 bx r1 
@@ -30,7 +34,8 @@ bx r1
 
 
 DontInterruptMovement: 
-pop {r2-r3} 
+pop {r1-r3}
+mov r12, r1  
 pop {r1} 
 ldr r1, =0x801A8B5
 bx r1 
--- WalkCalcLoop/C_Code.c
+++ WalkCalcLoop/C_Code.c
@@ -1,7 +1,75 @@
 #include "C_Code.h"
 
-int WalkCalcLoop(void)
+extern u8 TerrainThatCallsRangeEvent[];
+extern u8 TrapsThatCallRangeEvent[];
+extern u8 TerrainThatInterruptsMovement[];
+extern u8 TrapsThatInterruptMovement[];
+
+void RunRangeEventAt(struct Unit * unit, int x, int y);
+int WalkCalcLoop(struct Unit * unit, int x, int y)
 {
 
+    int terrain = gBmMapTerrain[y][x];
+    u8 * data;
+    for (data = TerrainThatCallsRangeEvent; *data; ++data)
+    {
+        if (terrain == *data)
+        {
+            RunRangeEventAt(unit, x, y);
+            break;
+        }
+    }
+
+    int trap = GetTrapAt(x, y)->type;
+    for (data = TrapsThatCallRangeEvent; *data; ++data)
+    {
+        if (trap == *data)
+        {
+            RunRangeEventAt(unit, x, y);
+            break;
+        }
+    }
+
+    if (gBmMapUnit[y][x])
+    {
+        return false; // unit there, so don't interrupt movement
+    }
+    for (data = TerrainThatInterruptsMovement; *data; ++data)
+    {
+        if (terrain == *data)
+        {
+            return true;
+        }
+    }
+    for (data = TrapsThatInterruptMovement; *data; ++data)
+    {
+        if (trap == *data)
+        {
+            return true;
+        }
+    }
     return false;
+}
+
+void RunRangeEventAt(struct Unit * unit, int x, int y)
+{
+    int xCur = unit->xPos;
+    int yCur = unit->yPos;
+    int actX = gActionData.xMove;
+    int actY = gActionData.yMove;
+    gActionData.xMove = x;
+    gActionData.yMove = y;
+    unit->xPos = x;
+    unit->yPos = y;
+    // if (StartAfterUnitMovedEvent())
+    if (CheckForWaitEvents())
+    {
+        // brk;
+        RunWaitEvents();
+    }
+
+    unit->xPos = xCur;
+    unit->yPos = yCur;
+    gActionData.xMove = actX;
+    gActionData.yMove = actY;
 }
