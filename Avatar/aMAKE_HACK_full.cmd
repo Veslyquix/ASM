b1fee093569694871530cc1b97ff0bedfe194d6e
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-01-30 10:11:16 -0500

    wip

---
+++ Avatar/aMAKE_HACK_full.cmd
@@ -0,0 +1,52 @@
+@echo off
+
+
+set "source_rom=%~dp0fe8srr.gba"
+set "source_sym=%~dp0fe8srr.sym
+set "vanilla_rom=%~dp0FE8_clean.gba"
+
+set "main_event=%~dp0Installer.event"
+
+set "target_rom=%~dp0avatar.gba"
+set "target_ups=%~dp0avatar.ups"
+set "target_sym=%~dp0avatar.sym"
+
+@rem defining tools
+
+
+set "ups=%~dp0Tools\ups\ups"
+set symcombo=%~dp0Tools\sym\SymCombo.exe
+
+@rem set %~dp0 into a variable because batch is stupid and messes with it when using conditionals?
+
+set "base_dir=%~dp0"
+
+@rem do the actual building
+
+echo Copying ROM
+
+copy "%source_rom%" "%target_rom%"
+
+echo:
+echo Assembling
+
+cd "%base_dir%EventAssembler"
+ColorzCore A FE8 "-output:%target_rom%" "-input:%main_event%" --nocash-sym "--build-times"
+
+@rem  echo:
+@rem  echo Generating patch
+
+@rem  cd "%base_dir%"
+@rem  "%ups%" diff -b "%vanilla_rom%" -m "%target_rom%" -o "%target_ups%"
+
+
+echo:
+echo Generating sym file
+
+echo: | ( "%symcombo%" "%target_sym%" "%target_sym%" "%source_sym" )
+@rem echo: | ( "%symcombo%" "%target_sym%" "%target_sym%" "%base_dir%\Tools\sym\VanillaOffsets.sym" )
+
+echo:
+echo Done!
+
+pause
