23ac46cb7bb12f5653e61adf123c7d777b822726
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-02-17 11:06:52 -0500

    python installer automation

--- Avatar/GetPals.py
+++ Avatar/GetPals.py
@@ -2,7 +2,8 @@ import os
 from PIL import Image
 from collections import defaultdict
 
-PNG_FOLDER = "png"
+RECOLOURS_FOLDER = "portrait_recolours"
+HAIRSTYLES_FOLDER = "portrait_hairstyles"
 OUTPUT_FILE = "GeneratedInstaller.event"
 
 def rgb8_to_gba5(value):
@@ -43,9 +44,10 @@ def format_rgb(color):
 
 def main():
     # Group files by base name
-    groups = defaultdict(list)
+    recolours = defaultdict(list)
+    hairstyles = defaultdict(list)
 
-    for file in os.listdir(PNG_FOLDER):
+    for file in os.listdir(RECOLOURS_FOLDER):
         if not file.lower().endswith(".png"):
             continue
 
@@ -54,12 +56,66 @@ def main():
             continue  # skip malformed names
 
         base, variant = name.split("_", 1)
-        groups[base].append((variant, os.path.join(PNG_FOLDER, file)))
+        recolours[base].append((variant, os.path.join(RECOLOURS_FOLDER, file)))
 
+
+    for file in os.listdir(HAIRSTYLES_FOLDER):
+        if not file.lower().endswith(".png"):
+            continue
+
+        name = os.path.splitext(file)[0]
+        if "_" not in name:
+            continue  # skip 
+
+        base, variant = name.split("_", 1)
+        hairstyles[base].append((variant, os.path.join(HAIRSTYLES_FOLDER, file)))
+    i = 0
+    flag = 0
     with open(OUTPUT_FILE, "w") as out:
+        
+        total = 0
+        out.write(f"//Generated! (No need to edit this file manually) \n")
+        out.write(f"ALIGN 4\nDynamicPortraitTable:\n")
+        for base in sorted(hairstyles.keys()):
+            variants = hairstyles[base]
+            count = len(variants)
+            for c in range(count):
+                if total != 0:
+                    out.write(f"DynPor(StartingMug, StartingMug+{total}, StartingFlag+{total-1})\n")
+                total+= 1
+        out.write(f"WORD 0 0 //Terminator\n")
+        out.write(f"\nALIGN 4\nNumHairstyles:\n")
+        for base in sorted(hairstyles.keys()):
+            variants = hairstyles[base]
+            count = len(variants)
+            out.write(f"BYTE {count} //{base}\n")
+        out.write(f"BYTE 0 //Terminator\n")
+        total = 0
+        out.write(f"\nALIGN 4\nAvatarPortraits:\n")
+        for base in sorted(hairstyles.keys()):
+            variants = hairstyles[base]
+            count = len(variants)
+            out.write(f"SHORT StartingMug+{total} //{base}\n")
+            total += count
+        out.write(f"SHORT 0 //Terminator\n")
 
-        for base in sorted(groups.keys()):
-            variants = groups[base]
+        total = 0
+        out.write(f"\nALIGN 4\nMugFlags:\n")
+        for base in sorted(hairstyles.keys()):
+            variants = hairstyles[base]
+            count = len(variants)
+            out.write(f"SHORT StartingFlag+{total} //{base}\n")
+            total += count
+        total -= 1
+        out.write(f"SHORT 0 //Terminator\n")
+        out.write(f"\nALIGN 4\nPortraitPalReplacements:\n")
+        
+        
+ 
+        hairFlags = []
+        skinFlags = []
+        for base in sorted(recolours.keys()):
+            variants = recolours[base]
             count = len(variants)
 
             # Find the Base variant first
@@ -72,11 +128,13 @@ def main():
             if base_palette is None:
                 print(f"Warning: No base variant found for {base}")
                 continue
+            
 
             for variant, path in sorted(variants):
                 colors = get_palette_colors(path)
 
                 output_entries = []
+                allBase = True
 
                 for color, base_color in zip(colors, base_palette):
                     if color == base_color:
@@ -84,21 +142,41 @@ def main():
                     else:
                         r, g, b = color
                         output_entries.append(f"rgb({r}, {g}, {b})")
+                        allBase = False
 
+                if allBase:
+                    continue
                 rgb_list = ", ".join(output_entries)
+                flag = total + i
 
                 line = (
-                    f"PortraitPals({base}_Base, "
-                    #f"{base}_Base+{count}, "
-                    f"{base}_Base+NumOfPortraits, "
-                    f"{base}Flag_{variant}, "
+                    f"PortraitPals(StartingMug, "
+                    f"StartingMug+{total}, "
+                    #f"{base}Flag_{variant}, "
+                    f"StartingFlag+{flag}, "
                     f"{rgb_list})"
                 )
+                if "skin" in variant.lower():
+                    skinFlags.append(flag)
+                else:
+                    hairFlags.append(flag)
+                i+=1
 
                 out.write(line + "\n")
 
-
-    print("Done! GeneratedInstaller.event created.")
+        out.write(f"SHORT 0xFFFF 0xFFFF 0xFFFF 0xFFFF 0xFFFF 0xFFFF 0xFFFF 0xFFFF 0xFFFF 0xFFFF // Terminator\n")
+        out.write(f"\nALIGN 4\nHairColFlags:\n")
+        for h in range(len(hairFlags)):
+            var = hairFlags[h]
+            out.write(f"SHORT StartingFlag+{var}\n")
+        out.write(f"SHORT 0 //Terminator\n")
+        out.write(f"\nALIGN 4\nSkinColFlags:\n")
+        for s in range(len(skinFlags)):
+            var = skinFlags[s]
+            out.write(f"SHORT StartingFlag+{var}\n")
+        out.write(f"SHORT 0 //Terminator\n")
+    flag = hex(flag)
+    print(f"Done! GeneratedInstaller.event created.\n{flag} global flags used.")
 
 
 if __name__ == "__main__":
