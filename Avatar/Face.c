6ce0f393c15cf440c7cbf0abfb1bb31fe96afb8a
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-02-18 10:33:04 -0500

    working...

--- Avatar/Face.c
+++ Avatar/Face.c
@@ -1,5 +1,5 @@
 
-struct FaceData
+struct FaceDataWithPad
 {
     /* 00 */ const u8 * img;
     /* 04 */ const u8 * imgChibi;
@@ -9,7 +9,17 @@ struct FaceData
     /* 14 */ u8 xMouth, yMouth;
     /* 16 */ u8 xEyes, yEyes;
     /* 18 */ u8 blinkKind;
+    u8 pad[7]; // make it size 0x20 which fixes issues with ram overlapping fsr
 };
+
+#define NumFaceDataSlots 8
+#define PaletteSpace (NumFaceDataSlots * 0x10)
+extern struct FaceDataWithPad FaceDataRam[NumFaceDataSlots]; // 1. 0x1C bytes ram * NumFaceDataSlots for the most
+                                                             // recent calls to GetPortraitData
+extern u16 FacePalRam[]; // 2. 0x20 bytes ram * NumFaceDataSlots for the 16 colour palette
+// extern u16 FacePalRam[0x10 * NumFaceDataSlots]; // 3. fids
+// extern u16 FacePalRam[0x10 * NumFaceDataSlots + NumFaceDataSlots]; // 3. slots
+
 // Because users repoint these tables, use pointers to them instead of the vanilla address of tables
 extern struct FaceData const * const sPortrait_data;
 
@@ -27,10 +37,6 @@ struct PortraitPalReplacementStruct
 };
 extern struct PortraitPalReplacementStruct PortraitPalReplacements[];
 
-extern struct FaceData Font_Sio_02000C60_Reused[4];
-
-extern u16 Sio_02000C80_Reused[];
-
 extern struct FaceData const * DynamicPortraits(int id);
 int GetAdjustedPortraitID(int fid)
 {
@@ -38,8 +44,10 @@ int GetAdjustedPortraitID(int fid)
     int newFid = ((int)data - (int)GetMugData(0)) / 0x1C; // calc which ID it is now
     return newFid;
 }
-
-int FindSlotFromFid2(int fid)
+/*
+// This didn't work, but searching through multiple instances of the same proc script
+// could be useful in the future
+int FindSlotFromLoadedFids(int fid)
 {
     struct FaceProc * face;
     struct ProcFindIterator procIter;
@@ -57,21 +65,55 @@ int FindSlotFromFid2(int fid)
             return i;
         }
     }
+    return (-1);
+}
+*/
+
+int IsFidCurrentlyLoaded(int fid)
+{
+    struct FaceProc * face;
+    struct ProcFindIterator procIter;
+
+    Proc_FindBegin(&procIter, gProcScr_E_FACE);
+
+    while ((face = Proc_FindNext(&procIter)))
+    {
+        if (face->faceId == fid)
+            return 1;
+    }
+
     return 0;
 }
-void UpdateMostRecentFids(int slot)
+
+void ClearUnusedFaceSlots(void)
 {
-    for (int i = 4; i > 0; --i)
+    int base = PaletteSpace;
+
+    for (int i = 0; i < NumFaceDataSlots; ++i)
     {
-        Sio_02000C80_Reused[0x44 + i] = Sio_02000C80_Reused[0x44 + i - 1];
+        int fid = FacePalRam[base + i];
+
+        if (fid != 0 && !IsFidCurrentlyLoaded(fid))
+        {
+            // Clear slot
+            FacePalRam[base + i] = 0;
+
+            // Optional: clear palette memory here too
+            // CpuFill16(0, PaletteRamAddressForSlot(i), 0x20);
+        }
     }
-    Sio_02000C80_Reused[0x44] = slot;
 }
 
-int IsSlotValid(int slot)
+void UpdateMostRecentFids(int slot)
 {
+    int base = PaletteSpace + NumFaceDataSlots;
 
-    return slot > 0 && slot < 4;
+    // Shift down
+    for (int i = NumFaceDataSlots - 1; i > 0; --i)
+        FacePalRam[base + i] = FacePalRam[base + i - 1];
+
+    // Put newest in front
+    FacePalRam[base + 0] = slot;
 }
 
 // I don't know if this makes sense
@@ -79,39 +121,38 @@ int IsSlotValid(int slot)
 // recent results from GetPortraitData
 int FindSlotFromFid(int fid)
 {
-    int tmp;
-    for (int i = 0; i < 4; ++i)
+    int base = PaletteSpace;
+    ClearUnusedFaceSlots();
+    int slot;
+    // Check if fid already loaded
+    for (int i = 0; i < NumFaceDataSlots; ++i)
     {
-        tmp = Sio_02000C80_Reused[0x40 + i];
-        if (tmp == fid)
+        if (FacePalRam[base + i] == fid)
         {
             UpdateMostRecentFids(i);
             return i;
         }
     }
 
-    int slot;
-
-    for (int i = 0; i < 4; ++i)
+    // Look for empty slot
+    for (int i = 0; i < NumFaceDataSlots; ++i)
     {
-
-        slot = Sio_02000C80_Reused[0x44 + i];
-        if (!IsSlotValid(slot))
+        if (FacePalRam[base + i] == 0)
         {
-            slot = i;
-        }
-        if (!Sio_02000C80_Reused[0x40 + slot])
-        {
-            // brk;
-            Sio_02000C80_Reused[0x40 + slot] = fid;
-            UpdateMostRecentFids(slot);
-            return slot;
+            FacePalRam[base + i] = fid;
+            UpdateMostRecentFids(i);
+            return i;
         }
     }
 
-    UpdateMostRecentFids(3);
+    // No empty slot, reuse least recently used
+    int lruBase = base + NumFaceDataSlots;
+    slot = FacePalRam[lruBase + NumFaceDataSlots - 1];
 
-    return 3;
+    FacePalRam[base + slot] = fid;
+    UpdateMostRecentFids(slot);
+
+    return slot;
 }
 
 const struct FaceData * NewGetPortraitData(int fid)
@@ -121,15 +162,17 @@ const struct FaceData * NewGetPortraitData(int fid)
     // const struct FaceData * data = GetMugData(fid);
     const struct FaceData * data = DynamicPortraits(fid);
     int newFid = ((int)data - (int)GetMugData(0)) / 0x1C; // calc which ID it is now
+    brk;
     // so we don't have to edit the DynamicPortraits asm
     int faceSlot = FindSlotFromFid(fid);
     // brk;
     int palOffset = 16 * faceSlot;
 
-    CpuFastCopy(data, (void *)&Font_Sio_02000C60_Reused[faceSlot], 0x1C); // src, dst, bytes
-    CpuFastCopy(data->pal, (void *)&Sio_02000C80_Reused[palOffset], 0x10);
+    CpuFastCopy(data, (void *)&FaceDataRam[faceSlot], 0x1C); // src, dst, bytes
+    // brk;
+    CpuFastCopy(data->pal, (void *)&FacePalRam[palOffset], 0x10);
     const u16 * basePal;
-    Font_Sio_02000C60_Reused[faceSlot].pal = (void *)&Sio_02000C80_Reused[palOffset];
+    FaceDataRam[faceSlot].pal = (void *)&FacePalRam[palOffset];
     struct PortraitPalReplacementStruct * palReplacements = &PortraitPalReplacements[0];
     int tmp;
     while (palReplacements->fidA && palReplacements->fidA != 0xFFFF)
@@ -138,19 +181,19 @@ const struct FaceData * NewGetPortraitData(int fid)
         if (palReplacements->fidA <= newFid && palReplacements->fidB >= newFid && CheckFlag(palReplacements->flag))
         {
             basePal = data->pal;
-            Sio_02000C80_Reused[palOffset] = 0xFFFF;
+            FacePalRam[palOffset] = 0xFFFF;
             for (int i = 0; i < 15; ++i)
             {
                 // brk;
                 tmp = palReplacements->pal[i];
                 if (tmp != (-1) && basePal[i + 1] != tmp)
                 {
-                    Sio_02000C80_Reused[palOffset + i + 1] = tmp; // don't adjust pal ID 0, as it is transparent
+                    FacePalRam[palOffset + i + 1] = tmp; // don't adjust pal ID 0, as it is transparent
                 }
             }
         }
         palReplacements++;
     }
     // brk;
-    return &Font_Sio_02000C60_Reused[faceSlot];
+    return (void *)&FaceDataRam[faceSlot];
 }
