1164095563c6264c88d9dfbd7b44f46da7c3642f
Veslyquix <74559742+Veslyquix@users.noreply.github.com>
2026-03-19 13:26:42 -0400

    release

--- GameOverNewGame/C_Code.c
+++ GameOverNewGame/C_Code.c
@@ -5,22 +5,72 @@ const struct ProcCmd VeslyGameOverWrapper[];
 const struct ProcCmd VeslyGameOverProc[];
 const struct ProcCmd VanillaGameOverProcCopy[];
 extern const u16 * GameOverEventTable[];
+extern int InitWM;
+extern int InitSupply;
+extern int InitChars;
+extern int InitTime;
+extern int InitGold;
+int ShouldInitAll(void)
+{
 
+    return InitWM && InitSupply && InitChars && InitTime && InitGold;
+}
 static void sub_8009C5C_(void)
 {
-    InitPlayConfig(0, 0);                           // gPlaySt stuff like difficulty
-    gPlaySt.chapterStateBits |= PLAY_FLAG_TUTORIAL; // Pre-Eirika/Ephraim route split
-    ResetPermanentFlags();
+    int gold = gPlaySt.partyGoldAmount;
+    int id = (-1);
+    struct PlaySt play_st;
+
+    if (IsValidSuspendSave(SAVE_ID_SUSPEND))
+    {
+        ReadSuspendSavePlaySt(SAVE_ID_SUSPEND, &play_st);
+
+        id = play_st.gameSaveSlot;
+    }
+    if (id < 0)
+    {
+        id = ReadLastGameSaveId();
+    }
+    int isTutorial = gPlaySt.config.controller; // see InitPlayConfig
+    int isDifficult = gPlaySt.chapterStateBits & PLAY_FLAG_HARD;
+    if (InitWM)
+        GmDataInit(); // wipe world map stuff
+
+    InitPlayConfig(isDifficult, isTutorial); // gPlaySt stuff like difficulty
+    // gPlaySt.chapterStateBits |= PLAY_FLAG_TUTORIAL; // Pre-Eirika/Ephraim route split
     ResetChapterFlags();
-    InitUnits();
+
+    // if (InitFlags) // WM stuff breaks without resetting flags
+    ResetPermanentFlags();
+
+    if (InitChars)
+        InitUnits();
+
     gPlaySt.chapterIndex = 0;
+
+    if (InitSupply)
+        ClearSupplyItems();
+
+    if (InitTime)
+        SetGameTime(0);
+
+    if (!InitGold)
+        gPlaySt.partyGoldAmount = gold;
+
+    if (ShouldInitAll())
+        WriteNewGameSave(id, isDifficult, 1, isTutorial);
+    else
+        WriteGameSave(id);
+
+    // WriteNewGameSave(int index, int isDifficult, int mode, int isTutorial)
+
     // gPlaySt.chapterIndex = proc->nextChapter;
     // ReadGameSave(ReadLastGameSaveId()); // added to restart the chapter for debugger
 }
 
 void VeslyStartNewGame(void)
 {
-    GmDataInit(); // wipe stuff ?
+
     struct GameCtrlProc * proc = Proc_Find(gProcScr_GameControl);
 
     proc->unk_2E = 20; // ?
@@ -58,7 +108,7 @@ void GameOver_HookCommon(ProcPtr proc)
 {
     EndAllMus();
     // SetNextGameActionId(GAME_ACTION_B); // GAME_ACTION_EVENT_RETURN
-    EndBMapMain();
+    //
     DeleteEventEngines();
 
     VeslyStartNewGame();
@@ -67,11 +117,20 @@ struct VeslyGameOver
 {
     PROC_HEADER;
     u8 fadedIn;
+    u8 chapterID;
 };
-int GameOver_Event(struct VeslyGameOver * proc)
+void GameOverInit(struct VeslyGameOver * proc)
 {
     proc->fadedIn = false;
-    CallEvent(GameOverEventTable[gPlaySt.chapterIndex], EV_EXEC_GAMEPLAY);
+    proc->chapterID = gPlaySt.chapterIndex;
+}
+int GameOver_Event(struct VeslyGameOver * proc)
+{
+    if (GameOverEventTable[proc->chapterID])
+    {
+        // CallEvent(GameOverEventTable[gPlaySt.chapterIndex], EV_EXEC_GAMEPLAY);
+        CallEvent(GameOverEventTable[proc->chapterID], EV_EXEC_QUIET);
+    }
     return 0;
 }
 
@@ -90,7 +149,6 @@ void VeslyEventEngineExists(struct VeslyGameOver * proc)
 {
     // check if the event engine fades to black. If it does, no need to fade after the event is done
     struct EventEngineProc * evProc = Proc_Find(ProcScr_StdEventEngine);
-    int fade = proc->fadedIn;
     if (evProc)
     {
 
@@ -126,18 +184,20 @@ const struct ProcCmd VeslyGameOverWrapper[] = {
 const struct ProcCmd VeslyGameOverProc[] = {
     PROC_NAME("VeslyGameOverProc"),
     PROC_YIELD,
+    PROC_CALL(GameOverInit),
+    PROC_CALL(GameOver_HookCommon),
     PROC_CALL(GameOver_Event),
     PROC_SLEEP(1),
     PROC_REPEAT(VeslyEventEngineExists),
-    // PROC_WHILE(EventEngineExists),
     PROC_CALL(GameOver_Fade),
     PROC_REPEAT(WaitForFade),
-    PROC_CALL(GameOver_HookCommon),
+    PROC_CALL(EndBMapMain), // this has to be after the event, otherwise the event breaks
     PROC_END,
 };
 
 const struct ProcCmd VanillaGameOverProcCopy[] = {
     PROC_YIELD,
+    PROC_CALL(GameOverInit),
     PROC_CALL(GameOver_Event),
     PROC_SLEEP(1),
     PROC_REPEAT(VeslyEventEngineExists),
@@ -158,3 +218,9 @@ const struct ProcCmd VanillaGameOverProcCopy[] = {
     PROC_CALL(PostGameOverHandler),
     PROC_END,
 };
+
+/*
+[OpenMidLeft][LoadFace][0x164][OpenMidLeft]Try placing units on [ToggleRed]Forts[ToggleRed]
+and healing with [ToggleRed]Vulneraries[ToggleRed].[A]
+Don't lose hope![A]
+*/
